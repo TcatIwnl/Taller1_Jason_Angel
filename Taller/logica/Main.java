3436f1fb355bbe9a803bf6c811c99c45fa56fdb7
TcatIwnl <angel.olivares@alumnos.ucn.cl>
2026-03-31 18:38:46 -0300

    Escritura de archivo

--- Taller/logica/Main.java
+++ Taller/logica/Main.java
@@ -1,63 +1,57 @@
-
 package logica;
 
 import java.util.Scanner;
 import java.io.*;
 
+public class Main {
 
-public class Main{
+    // Angel Eduardo Olivares Flores
+    // 22.338.590-7 / ICCI
 
-//Angel Eduardo Olivares Flores
-//22.338.590-7 / ICCI
+    // Jason Alexander Tapia Castro
+    // 22.382.028-K / ICCI
 
-	
-//Jason Alexander Tapia Castro
-//22.382.028-K / ICCI
-	
-	    static int MAX_REGISTROS = 300;
-	    static int MAX_USUARIOS = 3;
+    static int MAX_REGISTROS = 300;
+    static int MAX_USUARIOS = 3;
 
-	    //lista para usuarios.txt
-	    static String[] usuariosId = new String[MAX_USUARIOS];
-	    static String[] usuariosPass = new String[MAX_USUARIOS];
-	    static int cantUsuarios = 0;
+    // lista para usuarios.txt
+    static String[] usuariosId = new String[MAX_USUARIOS];
+    static String[] usuariosPass = new String[MAX_USUARIOS];
+    static int cantUsuarios = 0;
 
-	    
-	    //lista para registros.txt
-	    static String[] regUsuario = new String[MAX_REGISTROS];
-	    static String[] regFecha = new String[MAX_REGISTROS];
-	    static int[] regHoras = new int[MAX_REGISTROS];
-	    static String[] regActividad = new String[MAX_REGISTROS];
-	    static int cantRegistros = 0;
-	
-	
-    public static void main(String[] args) throws FileNotFoundException {
+    // lista para registros.txt
+    static String[] regUsuario = new String[MAX_REGISTROS];
+    static String[] regFecha = new String[MAX_REGISTROS];
+    static int[] regHoras = new int[MAX_REGISTROS];
+    static String[] regActividad = new String[MAX_REGISTROS];
+    static int cantRegistros = 0;
+
+    public static void main(String[] args) {
         Scanner lector = new Scanner(System.in);
-        
+
         leerArchivoRegistros();
         leerArchivoUsuarios();
-        
+
         int opcionPrincipal = 0;
         int opcionUsuario = 0;
+        int opcionAnalisis = 0;
+
         String usuario;
         String contrasena;
-        boolean accesoCorrecto = false;
-        
-        
-        
+        int posicionUsuario;
+
         do {
             System.out.println("1) Menu de Usuarios");
             System.out.println("2) Menu de Analisis");
             System.out.println("3) Salir");
             System.out.print("Ingrese una opcion: ");
-            
-            
+
             try {
-            	opcionPrincipal = Integer.parseInt(lector.nextLine());
-			} catch (Exception e) {
-				System.out.println("Error");
-			}
-           
+                opcionPrincipal = Integer.parseInt(lector.nextLine());
+            } catch (Exception e) {
+                System.out.println("Error al ingresar la opcion.");
+                opcionPrincipal = 0;
+            }
 
             switch (opcionPrincipal) {
                 case 1:
@@ -67,17 +61,9 @@ public class Main{
                     System.out.print("Contraseña: ");
                     contrasena = lector.nextLine();
 
-                    accesoCorrecto = false;
+                    posicionUsuario = login(usuario, contrasena);
 
-                    if (usuario.equals("Martin") && contrasena.equals("papurri")) {
-                        accesoCorrecto = true;
-                    } else if (usuario.equals("Catalina") && contrasena.equals("furryfacto")) {
-                        accesoCorrecto = true;
-                    } else if (usuario.equals("Estefania") && contrasena.equals("cutiemarks")) {
-                        accesoCorrecto = true;
-                    }
-
-                    if (accesoCorrecto) {
+                    if (posicionUsuario != -1) {
                         System.out.println("Acceso correcto!");
 
                         do {
@@ -91,24 +77,40 @@ public class Main{
                             System.out.println("4) Cambiar contraseña");
                             System.out.println("5) Salir");
                             System.out.print("Ingrese una opcion: ");
-                            opcionUsuario = Integer.parseInt(lector.nextLine());
+
+                            try {
+                                opcionUsuario = Integer.parseInt(lector.nextLine());
+                            } catch (Exception e) {
+                                System.out.println("Error al ingresar la opcion.");
+                                opcionUsuario = 0;
+                            }
 
                             switch (opcionUsuario) {
                                 case 1:
                                     System.out.println("Opcion registrar actividad seleccionada.");
+                                    registrarActividad(usuario, lector);
                                     break;
+
                                 case 2:
-                                    System.out.println("Opcion modificar actividad seleccionada.");
+                                    System.out.println("Cual actividad deseas modificar?");
+                                	// Opcion correcta, se actualiza con los registros añadidos
+                                    mostrarRegistrosDeUsuario(usuario);
                                     break;
+
                                 case 3:
-                                    System.out.println("Opcion eliminar actividad seleccionada.");
+                                    System.out.println("Cual actividad deseas eliminar?");
+                                	// Opcion correcta, se actualiza con los registros añadidos
+                                    mostrarRegistrosDeUsuario(usuario);
                                     break;
+
                                 case 4:
-                                    System.out.println("Opcion cambiar contraseña seleccionada.");
+                                    cambiarContrasena(usuario, lector);
                                     break;
+
                                 case 5:
                                     System.out.println("Saliendo del menu de usuario...");
                                     break;
+
                                 default:
                                     System.out.println("Opcion invalida.");
                                     break;
@@ -122,7 +124,53 @@ public class Main{
                     break;
 
                 case 2:
-                    System.out.println("Menu de analisis aun no implementado.");
+                    do {
+                        System.out.println();
+                        System.out.println("Bienvenido al menu de analisis!");
+                        System.out.println();
+                        System.out.println("Que deseas realizar?");
+                        System.out.println("1) Actividad mas realizada");
+                        System.out.println("2) Actividad mas realizada por cada usuario");
+                        System.out.println("3) Usuario con mayor procrastinacion");
+                        System.out.println("4) Ver todas las actividades");
+                        System.out.println("5) Salir");
+                        System.out.print("Ingrese una opcion: ");
+
+                        try {
+                            opcionAnalisis = Integer.parseInt(lector.nextLine());
+                        } catch (Exception e) {
+                            System.out.println("Error al ingresar la opcion.");
+                            opcionAnalisis = 0;
+                        }
+
+                        switch (opcionAnalisis) {
+                            case 1:
+                                System.out.println("Actividad mas realizada aun no implementada.");
+                                break;
+
+                            case 2:
+                                System.out.println("Actividad mas realizada por cada usuario aun no implementada.");
+                                break;
+
+                            case 3:
+                                System.out.println("Usuario con mayor procrastinacion aun no implementado.");
+                                break;
+
+                            case 4:
+                            	// Opcion correcta, se actualiza con los registros añadidos
+                                mostrarTodosLosRegistros();
+                                break;
+
+                            case 5:
+                                System.out.println("Saliendo del menu de analisis...");
+                                break;
+
+                            default:
+                                System.out.println("Opcion invalida.");
+                                break;
+                        }
+
+                    } while (opcionAnalisis != 5);
                     break;
 
                 case 3:
@@ -140,67 +188,249 @@ public class Main{
 
         lector.close();
     }
-    
-    //Funcion para leer el archivo de registros
+
+    // Funcion para leer el archivo de registros
     public static void leerArchivoRegistros() {
+        cantRegistros = 0;
+
         try {
             File registro = new File("Registros.txt");
             Scanner scRegistro = new Scanner(registro);
 
-            while(scRegistro.hasNextLine()) {
+            while (scRegistro.hasNextLine() && cantRegistros < MAX_REGISTROS) {
                 String linea = scRegistro.nextLine();
                 String[] partesRG = linea.split(";");
 
-                // Asignamos cada parte a su arreglo correspondiente
-                // usando cantRegistros para saber en que posicion guardarlo
-                
-                regUsuario[cantRegistros] = partesRG[0];
-                regFecha[cantRegistros] = partesRG[1];
-                
-                //Pasamos el texto a int
-                regHoras[cantRegistros] = Integer.parseInt(partesRG[2]); 
-                
-                regActividad[cantRegistros] = partesRG[3];
+                if (partesRG.length == 4) {
+                    regUsuario[cantRegistros] = partesRG[0];
+                    regFecha[cantRegistros] = partesRG[1];
+                    regHoras[cantRegistros] = Integer.parseInt(partesRG[2]);
+                    regActividad[cantRegistros] = partesRG[3];
 
-                // Ahora que ya guardamos todo, avanzamos el contador al siguiente espacio vacio
-                cantRegistros++; 
-                
+                    cantRegistros++;
+                }
             }
-            scRegistro.close(); 
+
+            scRegistro.close();
 
         } catch (Exception e) {
             System.out.println("Error al leer el archivo Registros.txt");
         }
     }
-    
-    //Funcion para leer el archivo de los usuarios
+
+    // Funcion para leer el archivo de los usuarios
     public static void leerArchivoUsuarios() {
+        cantUsuarios = 0;
+
         try {
             File archivoU = new File("Usuarios.txt");
-            
-            //verificar si el archivo existe
+
             if (archivoU.exists()) {
                 Scanner scUser = new Scanner(archivoU);
-                
-                // Leemos mientras haya lineas y no nos pasemos del limite
-                while (scUser.hasNextLine() && cantUsuarios <= MAX_USUARIOS) {
+
+                while (scUser.hasNextLine() && cantUsuarios < MAX_USUARIOS) {
                     String linea = scUser.nextLine();
                     String[] partesU = linea.split(";");
-                      
-                    usuariosId[cantUsuarios] = partesU[0];
-                    usuariosPass[cantUsuarios] = partesU[1];
-                        
-                    //contador de usuarios
-                    cantUsuarios++; 
+
+                    if (partesU.length == 2) {
+                        usuariosId[cantUsuarios] = partesU[0];
+                        usuariosPass[cantUsuarios] = partesU[1];
+
+                        cantUsuarios++;
+                    }
                 }
+
                 scUser.close();
             } else {
                 System.out.println("No se encontro Usuarios.txt");
             }
-            
+
         } catch (Exception e) {
             System.out.println("Error al cargar los usuarios.");
         }
     }
-}
 
+    // Funcion para validar login
+    public static int login(String usuario, String contrasena) {
+        for (int i = 0; i < cantUsuarios; i++) {
+            if (usuariosId[i].equals(usuario) && usuariosPass[i].equals(contrasena)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    // Funcion para mostrar solo los registros de un usuario
+    public static int mostrarRegistrosDeUsuario(String usuarioBuscado) {
+        int contador = 0;
+
+        System.out.println("0) Regresar");
+
+        for (int i = 0; i < cantRegistros; i++) {
+            if (regUsuario[i].equals(usuarioBuscado)) {
+                contador++;
+                System.out.println(contador + ") " + regUsuario[i] + ";" + regFecha[i] + ";" + regHoras[i] + ";" + regActividad[i]);
+            }
+        }
+
+        if (contador == 0) {
+            System.out.println("Este usuario no tiene actividades registradas.");
+        }
+
+        return contador;
+    }
+
+    // Funcion para guardar los indices reales de un usuario
+    // OPCIONES 2 Y 3 DEL MENU 1
+    public static int[] obtenerIndicesDeUsuario(String usuarioBuscado) {
+        int[] indices = new int[MAX_REGISTROS];
+        int j = 0;
+
+        for (int i = 0; i < cantRegistros; i++) {
+            if (regUsuario[i].equals(usuarioBuscado)) {
+                indices[j] = i;
+                j++;
+            }
+        }
+
+        return indices;
+    }
+
+    // Funcion para mostrar todos los registros
+    public static void mostrarTodosLosRegistros() {
+        if (cantRegistros == 0) {
+            System.out.println("No hay registros guardados.");
+        } else {
+            for (int i = 0; i < cantRegistros; i++) {
+                System.out.println((i + 1) + ") " + regUsuario[i] + ";" + regFecha[i] + ";" + regHoras[i] + ";" + regActividad[i]);
+            }
+        }
+    }
+    // Funcion menu 1, 1) Registrar actividad.
+    public static void registrarActividad(String usuarioActual, Scanner lector) {
+        String fecha;
+        int horas;
+        String actividad;
+
+        if (cantRegistros >= MAX_REGISTROS) {
+            System.out.println("No se pueden agregar mas registros.");
+            return;
+        }
+
+        System.out.print("Ingrese la fecha (dd/mm/yyyy): ");
+        fecha = lector.nextLine();
+
+        do {
+            System.out.print("Ingrese la cantidad de horas: ");
+            try {
+                horas = Integer.parseInt(lector.nextLine());
+
+                if (horas <= 0) {
+                    System.out.println("Las horas deben ser mayores a 0.");
+                }
+
+            } catch (Exception e) {
+                System.out.println("Error, ingrese un numero valido.");
+                horas = -1;
+            }
+        } while (horas <= 0);
+
+        System.out.print("Ingrese la actividad: ");
+        actividad = lector.nextLine();
+
+        regUsuario[cantRegistros] = usuarioActual;
+        regFecha[cantRegistros] = fecha;
+        regHoras[cantRegistros] = horas;
+        regActividad[cantRegistros] = actividad;
+
+        cantRegistros++;
+
+        guardarCambios(1);
+        
+        System.out.println("Actividad registrada con exito.");
+    }
+    // Funcion para guardar (sobreescribir) los arreglos en el archivo de texto
+    public static void guardarCambios(int opcion) {
+    	//Verificamos que tipo de guardado queremos para ahorar ram
+    	if (opcion == 1) {
+	        try {
+	            // Al no poner true al lado del nombre, le decimos a Java que 
+	            // borre el contenido anterior y escriba todo el archivo desde cero.
+	            FileWriter archivo = new FileWriter("Registros.txt");
+	            PrintWriter escritor = new PrintWriter(archivo);
+	            
+	            // Recorremos nuestros arreglos hasta donde haya datos reales
+	            for (int i = 0; i < cantRegistros; i++) {
+	                // Escribimos la linea respetando el formato original con los punto y coma
+	                escritor.println(regUsuario[i] + ";" + regFecha[i] + ";" + regHoras[i] + ";" + regActividad[i]);
+	            }
+	            // Cerramos el escritor para que el archivo no quede en blanco
+	            escritor.close();
+	            } catch (Exception e) {
+	            System.out.println("Error al guardar los datos en Registros.txt");
+	        }
+    	}
+    	
+    	else if(opcion == 2) {
+    		try {
+	        	FileWriter archivoUs = new FileWriter("Usuarios.txt");
+	            PrintWriter escritorUs = new PrintWriter(archivoUs);
+	
+	            //repetimos con los usuarios
+	            for (int i = 0; i < cantUsuarios; i++) {
+	            	
+	            	escritorUs.println(usuariosId[i] + ";" + usuariosPass[i]);
+	            }
+	            
+	            escritorUs.close();
+			} catch (Exception e) {
+				System.out.println("Error al guardar los datos en Usuarios.txt");
+			}
+    	}
+        
+            
+        
+    }
+    
+ // Menu 1, opcion 4) Cambiar contrasena
+    public static void cambiarContrasena(String usuarioActual, Scanner lector) {
+        int posicionUsuario = -1;
+        String nuevaContrasena;
+        String repetirContrasena;
+
+        // Recorro en busca de la posición
+        for (int i= 0; i < cantUsuarios; i++) {
+            if (usuariosId[i].equals(usuarioActual)) {
+                posicionUsuario = i;
+                break;
+            }
+        }
+
+        // Filtro por si no se encuentra en el arreglo
+        // Redudante, si ingreso es porque el usuario si existe, printeo solo en construccion de codigo
+        if (posicionUsuario == -1) {
+            System.out.println("Usuario no encontrado");
+            return;
+        }
+
+        System.out.print("Ingrese la nueva contraseña: ");
+        nuevaContrasena = lector.nextLine();
+
+        System.out.print("Repita la nueva contraseña: ");
+        repetirContrasena = lector.nextLine();
+
+        // Filtro por si no coinciden contraseñas
+        if (!nuevaContrasena.equals(repetirContrasena)) {
+            System.out.println("Las contraseñas no coinciden");
+        // Filtro invisible
+        } else if (nuevaContrasena.equals("")) {
+            System.out.println("La contraseña no puede estar vacia");
+        } else {
+            usuariosPass[posicionUsuario] = nuevaContrasena;
+            guardarCambios(2);
+            System.out.println("Contraseña cambiada con exito");
+            
+        }
+    }
+}
